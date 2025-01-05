fa08889750f985636e0533303726c29011e4e374
Kevin Agoh <kevinagoh@hotmail.fr>
2025-01-05 17:51:55 +0100

    Game creation done

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 import http from 'node:http';
 import { Server } from 'socket.io';
-
 const httpServer = http.createServer();
 
 const io = new Server(httpServer, {
   cors: { origin: '*' }
 });
 
+const games = {};
+const players = {};
+
 io.on('connection', socket => {
   console.log('a user connected:', socket.id);
+
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    handleDisconnect(socket);
+  });
+
+  socket.on('create-game', gameData => {
+    createGame(socket, gameData);
+    console.log('Game :', gameData);
   });
 });
 
+function handleDisconnect(socket) {
+  Object.values(games).forEach(game => {
+    game.players = game.players.filter(p => p.id !== socket.id);
+  });
+}
+
+function createGame(socket, gameData) {
+  const generateUniqueId = () => Math.random().toString(36).substring(2, 9);
+
+  const game = {
+    id: generateUniqueId(),
+    players: []
+  };
+  games[game.id] = game;
+  game.players.push({
+    id: socket.id,
+    name: gameData.name || 'Player'
+  });
+  socket.join(game.id);
+  socket.emit('game-created', game);
+}
 io.listen(3001);
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
 const socket = io('localhost:3001');
 
 function App(): ReactNode {
+  const [gameId, setGameId] = useState<string | null>(null);
+
+  const [players, setPlayers] = useState<string[]>([]);
+  const [playerName, setPlayerName] = useState('');
+
   useEffect(() => {
     socket.on('connect', () => {
       console.log('Connected to server');
@@ -13,13 +18,38 @@ function App(): ReactNode {
       console.log('Disconnected from server');
     });
 
+    socket.on('game-created', gameData => {
+      console.log('Game created', gameData);
+      setGameId(gameData.id);
+      setPlayers([...players, gameData.players]);
+    });
+
     return () => {
       socket.off('connect');
       socket.off('disconnect');
+      socket.off('game-created');
     };
-  }, []);
+  }, [players]);
 
-  return <></>;
+  const handleCreateGame = () => {
+    socket.emit('create-game', { name: playerName });
+  };
+
+  return (
+    <>
+      {!gameId && (
+        <div>
+          <input
+            type='text'
+            value={playerName}
+            onChange={e => setPlayerName(e.target.value)}
+            placeholder='Enter your name'
+          />
+          <button onClick={handleCreateGame}>Create Game</button>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
